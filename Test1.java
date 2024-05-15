443eb3cd79e86ec07a77e1395a3333913384dab6
sadhakunps <sadaqunps@gmail.com>
2024-05-15 22:48:16 +0530

    Basic programs

---
+++ Test1.java
@@ -0,0 +1,8 @@
+
+class Test1{
+	static int a=100;
+	public static void main(String[] args){
+		
+		System.out.println(a);
+	}
+}
---
+++ Test2.java
@@ -0,0 +1,12 @@
+class Test2{
+	public static void m1(){
+		System.out.println("Test class m1 method");
+	}
+	public void m2(){
+		System.out.println("Test class m2 method");
+	}
+	public static void main(String[] args){
+		m1();
+		System.out.println("Hello World");
+	}
+}
